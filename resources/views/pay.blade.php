9736f7f842bc9b99c55f2c5590cefd6d27c237e3
sisirrag <mca549@rajagiri.edu>
2021-07-18 03:05:02 +0530

    m

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -1,174 +1,188 @@
-<!DOCTYPE html>
-<html lang="en" >
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-<head>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
 
-  <meta charset="UTF-8">
-  
-<link rel="apple-touch-icon" type="image/png" href="https://cpwebassets.codepen.io/assets/favicon/apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png" />
-<meta name="apple-mobile-web-app-title" content="CodePen">
 
-<link rel="shortcut icon" type="image/x-icon" href="https://cpwebassets.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico" />
+<div class="container">
+<br>  <p class="text-center">More bootstrap 4 components on <a href="http://bootstrap-ecommerce.com/"> Bootstrap-ecommerce.com</a></p>
+<hr>
 
-<link rel="mask-icon" type="" href="https://cpwebassets.codepen.io/assets/favicon/logo-pin-8f3771b1072e3c38bd662872f6b673a722f4b3ca2421637d5596661b4e2132cc.svg" color="#111" />
-
-  <title>Payment</title>
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-
-  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>
-<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-  
-<style>
-body { margin:50px auto; width:600px;}
-
-/* CSS for Credit Card Payment form */
-.credit-card-box .panel-title {
-    display: inline;
-    font-weight: bold;
-}
-.credit-card-box .form-control.error {
-    border-color: red;
-    outline: 0;
-    box-shadow: inset 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(255,0,0,0.6);
-}
-.credit-card-box label.error {
-  font-weight: bold;
-  color: red;
-  padding: 2px 8px;
-  margin-top: 2px;
-}
-.credit-card-box .payment-errors {
-  font-weight: bold;
-  color: red;
-  padding: 2px 8px;
-  margin-top: 2px;
-}
-.credit-card-box label {
-    display: block;
-}
-/* The old "center div vertically" hack */
-.credit-card-box .display-table {
-    display: table;
-}
-.credit-card-box .display-tr {
-    display: table-row;
-}
-.credit-card-box .display-td {
-    display: table-cell;
-    vertical-align: middle;
-    width: 50%;
-}
-/* Just looks nicer */
-.credit-card-box .panel-heading img {
-    min-width: 180px;
-}
-</style>
-
-  <script>
-  window.console = window.console || function(t) {};
-</script>
-
-  
-  
-  <script>
-  if (document.location.search.match(/type=embed/gi)) {
-    window.parent.postMessage("resize", "*");
-  }
-</script>
-
-</head>
-
-<body translate="no" style="background-color:#cfcfcf">
-  <div class="container">
 <div class="row">
-<div  class="col col-12 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1">
-</div>
-<!-- You can make it whatever width you want. I'm making it full width
-on <= small devices and 4/12 page width on >= medium devices -->
+	<aside class="col-sm-6">
+<p>Payment form1</p>
 
-<div class="col-xs-12 col-md-4">
+<article class="card">
+<div class="card-body p-5">
+<p> <img src="http://bootstrap-ecommerce.com/main/images/icons/pay-visa.png"> 
+	<img src="http://bootstrap-ecommerce.com/main/images/icons/pay-mastercard.png"> 
+   <img src="http://bootstrap-ecommerce.com/main/images/icons/pay-american-ex.png">
+</p>
+<p class="alert alert-success">Some text success or error</p>
 
-<br><br><br>
-<!-- CREDIT CARD FORM STARTS HERE -->
-<div class="panel panel-default credit-card-box">
-<div class="panel-heading display-table" >
-<div class="row display-tr" >
-<h3 class="panel-title display-td" >Payment Details</h3>
-<div class="display-td" >                            
-<img src="images/about.jpg" width="25" height="20" class="img-responsive" />
-</div>
-</div>                    
-</div>
-<div class="panel-body">
-<form role="form" id="payment-form">
-<div class="row">
-<div class="col-xs-12">
+<form role="form">
 <div class="form-group">
-<label for="cardNumber">CARD NUMBER</label>
+<label for="username">Full name (on the card)</label>
 <div class="input-group">
-<input type="text" class="form-control" name="cardNumber" placeholder="Valid Card Number"  >
-<span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
-</div>
-</div>                            
-</div>
-</div>
-<div class="row">
-<div class="col-xs-7 col-md-7">
-<div class="form-group">
-<label for="cardExpiry"><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label>
-<input 
-type="tel" 
-class="form-control" 
-name="cardExpiry"
-placeholder="MM / YY"
-autocomplete="cc-exp"
+	<div class="input-group-prepend">
+		<span class="input-group-text"><i class="fa fa-user"></i></span>
+	</div>
+	<input type="text" class="form-control" name="username" placeholder="" required="">
+</div> <!-- input-group.// -->
+</div> <!-- form-group.// -->
 
->
-</div>
-</div>
-<div class="col-xs-5 col-md-5 pull-right">
 <div class="form-group">
-<label for="cardCVC">CV CODE</label>
-<input 
-type="tel" 
-class="form-control"
-name="cardCVC"
-placeholder="CVC"
-autocomplete="cc-csc"
+<label for="cardNumber">Card number</label>
+<div class="input-group">
+	<div class="input-group-prepend">
+		<span class="input-group-text"><i class="fa fa-credit-card"></i></span>
+	</div>
+	<input type="text" class="form-control" name="cardNumber" placeholder="">
+</div> <!-- input-group.// -->
+</div> <!-- form-group.// -->
 
->
-</div>
-</div>
-</div>
 <div class="row">
-<div class="col-xs-12">
-<div class="form-group">
-<label for="couponCode">OTP</label>
-<input type="text" class="form-control" name="couponCode">
-</div>
-</div>                        
-</div>
-<div class="row">
-<div class="col-xs-12">
-<form action="pays" method="post">
-            @csrf
-            <input type="hidden" name="id"  value="{{$info->id}}"/>       
-<center><button class="btn btn-success" type="submit">Pay Now</button> </center>
+    <div class="col-sm-8">
+        <div class="form-group">
+            <label><span class="hidden-xs">Expiration</span> </label>
+        	<div class="form-inline">
+        		<select class="form-control" style="width:45%">
+				  <option>MM</option>
+				  <option>01 - Janiary</option>
+				  <option>02 - February</option>
+				  <option>03 - February</option>
+				</select>
+	            <span style="width:10%; text-align: center"> / </span>
+	            <select class="form-control" style="width:45%">
+				  <option>YY</option>
+				  <option>2018</option>
+				  <option>2019</option>
+				</select>
+        	</div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="form-group">
+            <label data-toggle="tooltip" title="" data-original-title="3 digits code on back side of the card">CVV <i class="fa fa-question-circle"></i></label>
+            <input class="form-control" required="" type="text">
+        </div> <!-- form-group.// -->
+    </div>
+</div> <!-- row.// -->
+<button class="subscribe btn btn-primary btn-block" type="button"> Confirm  </button>
 </form>
+</div> <!-- card-body.// -->
+</article> <!-- card.// -->
+
+
+	</aside> <!-- col.// -->
+	<aside class="col-sm-6">
+<p>Paymetn form2</p>
+
+<article class="card">
+<div class="card-body p-5">
+
+<ul class="nav bg-light nav-pills rounded nav-fill mb-3" role="tablist">
+	<li class="nav-item">
+		<a class="nav-link active" data-toggle="pill" href="#nav-tab-card">
+		<i class="fa fa-credit-card"></i> Credit Card</a></li>
+	<li class="nav-item">
+		<a class="nav-link" data-toggle="pill" href="#nav-tab-paypal">
+		<i class="fab fa-paypal"></i>  Paypal</a></li>
+	<li class="nav-item">
+		<a class="nav-link" data-toggle="pill" href="#nav-tab-bank">
+		<i class="fa fa-university"></i>  Bank Transfer</a></li>
+</ul>
+
+<div class="tab-content">
+<div class="tab-pane fade show active" id="nav-tab-card">
+	<p class="alert alert-success">Some text success or error</p>
+	<form role="form">
+	<div class="form-group">
+		<label for="username">Full name (on the card)</label>
+		<input type="text" class="form-control" name="username" placeholder="" required="">
+	</div> <!-- form-group.// -->
+
+	<div class="form-group">
+		<label for="cardNumber">Card number</label>
+		<div class="input-group">
+			<input type="text" class="form-control" name="cardNumber" placeholder="">
+			<div class="input-group-append">
+				<span class="input-group-text text-muted">
+					<i class="fab fa-cc-visa"></i>   <i class="fab fa-cc-amex"></i>   
+					<i class="fab fa-cc-mastercard"></i> 
+				</span>
+			</div>
+		</div>
+	</div> <!-- form-group.// -->
+
+	<div class="row">
+	    <div class="col-sm-8">
+	        <div class="form-group">
+	            <label><span class="hidden-xs">Expiration</span> </label>
+	        	<div class="input-group">
+	        		<input type="number" class="form-control" placeholder="MM" name="">
+		            <input type="number" class="form-control" placeholder="YY" name="">
+	        	</div>
+	        </div>
+	    </div>
+	    <div class="col-sm-4">
+	        <div class="form-group">
+	            <label data-toggle="tooltip" title="" data-original-title="3 digits code on back side of the card">CVV <i class="fa fa-question-circle"></i></label>
+	            <input type="number" class="form-control" required="">
+	        </div> <!-- form-group.// -->
+	    </div>
+	</div> <!-- row.// -->
+	<button class="subscribe btn btn-primary btn-block" type="button"> Confirm  </button>
+	</form>
+</div> <!-- tab-pane.// -->
+<div class="tab-pane fade" id="nav-tab-paypal">
+<p>Paypal is easiest way to pay online</p>
+<p>
+<button type="button" class="btn btn-primary"> <i class="fab fa-paypal"></i> Log in my Paypal </button>
+</p>
+<p><strong>Note:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. </p>
 </div>
+<div class="tab-pane fade" id="nav-tab-bank">
+<p>Bank account details</p>
+<dl class="param">
+  <dt>BANK: </dt>
+  <dd> THE WORLD BANK</dd>
+</dl>
+<dl class="param">
+  <dt>Accaunt number: </dt>
+  <dd> 12345678912345</dd>
+</dl>
+<dl class="param">
+  <dt>IBAN: </dt>
+  <dd> 123456789</dd>
+</dl>
+<p><strong>Note:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. </p>
+</div> <!-- tab-pane.// -->
+</div> <!-- tab-content .// -->
+
+</div> <!-- card-body.// -->
+</article> <!-- card.// -->
+
+
+	</aside> <!-- col.// -->
+</div> <!-- row.// -->
+
+</div> 
+<!--container end.//-->
+
+<br><br>
+<article class="bg-secondary mb-3">  
+<div class="card-body text-center">
+
+    <h3 class="text-white mt-3">Bootstrap 4 UI KIT</h3>
+<p class="h5 text-white">Components and templates  <br> for Ecommerce, marketplace, booking websites 
+and product landing pages</p>   <br>
+<p><a class="btn btn-warning" target="_blank" href="http://bootstrap-ecommerce.com/"> Bootstrap-ecommerce.com  
+ <i class="fa fa-window-restore "></i></a></p>
 </div>
-<div class="row" style="display:none;">
-<div class="col-xs-12">
-<p class="payment-errors"></p>
-</div>
-</div>
-</form>
-<div  class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-</div>
-</div>
-</div>            
-<!-- CREDIT CARD FORM ENDS HERE -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>       
-</body>
-</html>
+<br><br>
+</article>
