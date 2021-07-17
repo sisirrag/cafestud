9960b5a8350c27f473f68d60e7120210d34da6ba
sisirrag <mca549@rajagiri.edu>
2021-07-18 00:21:18 +0530

    m

--- resources/views/sfee.blade.php
+++ resources/views/sfee.blade.php
@@ -82,7 +82,7 @@
                                     </div>
                                 </div>
                                 <div class="menu-item-description">
-                                    <p>Amount: Rs.{{ $row->fee }}&nbsp;&nbsp;<a class="btn btn-style btn-white " href="/">Pay Now</a></p>
+                                    <p>Amount: Rs.{{ $row->fee }}&nbsp;&nbsp;<a class="btn btn-style btn-white btn-small text-right" href="/">Pay Now</a></p>
                                 </div>
                                 <div class="menu-item-description">
                                     <p></p>
